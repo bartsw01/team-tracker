b8349f58217894c12c177651906d2afff352e2dc
Bart Webber <bart@aseenrichment.com>
2017-08-11 11:44:25 -0700

    add getter and setter for Team constructor

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -14,6 +14,8 @@ public class Team {
     private int id;
     private boolean addedTeam;
 
+
+
     public Team (String teamName, String teamMember1, String teamMember2, String teamMember3, String teamMember4){
         this.teamName = teamName;
         this.teamMember1 = teamMember1;
@@ -25,4 +27,78 @@ public class Team {
         this.id = allTeams.size();
         this.addedTeam = false;
     }
+
+    public static ArrayList<Team> getAllTeams() {
+        return allTeams;
+    }
+
+    public static void setAllTeams(ArrayList<Team> allTeams) {
+        Team.allTeams = allTeams;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getTeamMember1() {
+        return teamMember1;
+    }
+
+    public void setTeamMember1(String teamMember1) {
+        this.teamMember1 = teamMember1;
+    }
+
+    public String getTeamMember2() {
+        return teamMember2;
+    }
+
+    public void setTeamMember2(String teamMember2) {
+        this.teamMember2 = teamMember2;
+    }
+
+    public String getTeamMember3() {
+        return teamMember3;
+    }
+
+    public void setTeamMember3(String teamMember3) {
+        this.teamMember3 = teamMember3;
+    }
+
+    public String getTeamMember4() {
+        return teamMember4;
+    }
+
+    public void setTeamMember4(String teamMember4) {
+        this.teamMember4 = teamMember4;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isAddedTeam() {
+        return addedTeam;
+    }
+
+    public void setAddedTeam(boolean addedTeam) {
+        this.addedTeam = addedTeam;
+    }
+
+
 }
